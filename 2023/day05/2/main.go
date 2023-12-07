d5db342995fb898b955132a14afbe99f205c104c
JaingoUnchaind <bhavya5jain@gmail.com>
2023-12-07 11:55:15 +0900

    day 5 part 2 improvement

--- 2023/day05/2/main.go
+++ 2023/day05/2/main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 )
 
@@ -19,147 +17,138 @@ func printStringList(input []string) {
 }
 
 type S2DMap struct {
-	sourceRangeStart      int
 	destinationRangeStart int
-	rangeLength           int
+	sourceRange           Range
 }
 
-type SeedRange struct {
-	seedStart   int
-	rangeLength int
+type Range struct {
+	start  int
+	length int
 }
 
-func parseS2DMap(input []string) (s2dMap []S2DMap) {
-	s2dMap = make([]S2DMap, 0)
-
-	for _, line := range input {
-		values := strings.Fields(line)
-		sourceRangeStart, _ := strconv.Atoi(values[1])
-		destinationRangeStart, _ := strconv.Atoi(values[0])
-		rangeLength, _ := strconv.Atoi(values[2])
-		s2dMap = append(s2dMap, S2DMap{
-			sourceRangeStart,
-			destinationRangeStart,
-			rangeLength,
-		})
-	}
-	return
-}
-
-func findEmptyLines(input []string) int {
-	for i, line := range input {
-		if line == "" {
-			return i
-		}
-	}
-
-	return -1
-}
-
-func parseInput(input []string) ([]SeedRange, []S2DMap, []S2DMap, []S2DMap, []S2DMap, []S2DMap, []S2DMap, []S2DMap) {
+func parseInput(input []string) ([]Range, [][]S2DMap) {
 	seedsString := strings.Split(input[0], ":")[1]
 	seedsIntermediate := strings.Fields(seedsString)
-	seedRange := make([]SeedRange, len(seedsIntermediate)/2)
-	for i := 0; i < len(seedRange); i++ {
+	seedRanges := make([]Range, len(seedsIntermediate)/2)
+	for i := 0; i < len(seedRanges); i++ {
 		seedsStart, _ := strconv.Atoi(seedsIntermediate[i*2])
 		rangeLength, _ := strconv.Atoi(seedsIntermediate[i*2+1])
-		seedRange[i] = SeedRange{
+		seedRanges[i] = Range{
 			seedsStart,
 			rangeLength,
 		}
 	}
 
 	input = input[3:]
+	input = append(input, "")
+	s2dMapList := make([][]S2DMap, 0)
+	s2dMap := make([]S2DMap, 0)
+	for i := 0; i < len(input); i++ {
+		if input[i] == "" {
+			s2dMapList = append(s2dMapList, s2dMap)
+			s2dMap = make([]S2DMap, 0)
+			i++
+			continue
+		}
 
-	emptyLine := findEmptyLines(input)
-	seedToSoilMap := parseS2DMap(input[:emptyLine])
-	input = input[emptyLine+2:]
-
-	emptyLine = findEmptyLines(input)
-	soilToFertilizerMap := parseS2DMap(input[:emptyLine])
-	input = input[emptyLine+2:]
-
-	emptyLine = findEmptyLines(input)
-	fertilizerToWaterMap := parseS2DMap(input[:emptyLine])
-	input = input[emptyLine+2:]
-
-	emptyLine = findEmptyLines(input)
-	waterToLightMap := parseS2DMap(input[:emptyLine])
-	input = input[emptyLine+2:]
-
-	emptyLine = findEmptyLines(input)
-	lightToTemperatureMap := parseS2DMap(input[:emptyLine])
-	input = input[emptyLine+2:]
-
-	emptyLine = findEmptyLines(input)
-	temperatureToHumidityMap := parseS2DMap(input[:emptyLine])
-	input = input[emptyLine+2:]
-
-	humidityToLocationMap := parseS2DMap(input)
-
-	return seedRange, seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
+		values := strings.Fields(input[i])
+		sourceRangeStart, _ := strconv.Atoi(values[1])
+		destinationRangeStart, _ := strconv.Atoi(values[0])
+		rangeLength, _ := strconv.Atoi(values[2])
+		s2dMap = append(s2dMap, S2DMap{
+			destinationRangeStart,
+			Range{
+				sourceRangeStart,
+				rangeLength,
+			},
+		})
 	}
-	return b
+
+	return seedRanges, s2dMapList
 }
 
-func findDestination(seed int, s2dmap []S2DMap) int {
-	for _, mapValue := range s2dmap {
-		if seed >= mapValue.sourceRangeStart && seed < (mapValue.sourceRangeStart+mapValue.rangeLength) {
-			return (seed - mapValue.sourceRangeStart) + mapValue.destinationRangeStart
+func Max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func Min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
+func minRange(ranges ...Range) Range {
+	if len(ranges) == 0 {
+		return Range{}
+	}
+
+	minRange := ranges[0]
+	for _, r := range ranges {
+		if r.start < minRange.start {
+			minRange = r
 		}
 	}
 
-	return seed
+	return minRange
 }
 
 func youGiveASeedAFertilizer(input []string) {
 	startTime := time.Now()
 
-	seedRange, seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap := parseInput(input)
+	seedRanges, s2dMapList := parseInput(input)
 
-	minimumLocation := math.MaxInt
+	sourceRanges := seedRanges
+	for _, s2dMap := range s2dMapList {
+		destinationRanges := make([]Range, 0)
 
-	ch := make(chan int)
-	var wg sync.WaitGroup
+		for len(sourceRanges) > 0 {
+			seedRange := sourceRanges[len(sourceRanges)-1]
+			sourceRanges = sourceRanges[:len(sourceRanges)-1]
 
-	for _, seedr := range seedRange {
-		wg.Add(1)
-		go func(seedr SeedRange, ch chan<- int, wg *sync.WaitGroup) {
-			defer wg.Done()
-			minLocation := math.MaxInt
+			flag := false
+			for _, s2d := range s2dMap {
+				divideStart := Max(seedRange.start, s2d.sourceRange.start)
+				divideEnd := Min(seedRange.start+seedRange.length, s2d.sourceRange.start+s2d.sourceRange.length)
 
-			for seed := seedr.seedStart; seed < (seedr.seedStart + seedr.rangeLength); seed++ {
-				soil := findDestination(seed, seedToSoilMap)
-				fertilizer := findDestination(soil, soilToFertilizerMap)
-				water := findDestination(fertilizer, fertilizerToWaterMap)
-				light := findDestination(water, waterToLightMap)
-				temperature := findDestination(light, lightToTemperatureMap)
-				humidity := findDestination(temperature, temperatureToHumidityMap)
-				location := findDestination(humidity, humidityToLocationMap)
+				if divideStart < divideEnd {
+					destinationRanges = append(destinationRanges, Range{
+						divideStart - s2d.sourceRange.start + s2d.destinationRangeStart,
+						divideEnd - divideStart,
+					})
 
-				minLocation = min(location, minLocation)
+					if divideStart > seedRange.start {
+						sourceRanges = append(sourceRanges, Range{
+							seedRange.start,
+							divideStart - seedRange.start,
+						})
+					}
+
+					if divideEnd < seedRange.start+seedRange.length {
+						sourceRanges = append(sourceRanges, Range{
+							divideEnd,
+							seedRange.start + seedRange.length - divideEnd,
+						})
+					}
+					flag = true
+					break
+				}
 			}
-			ch <- minLocation
-			fmt.Println(seedr)
-		}(seedr, ch, &wg)
-
+			if flag == false {
+				destinationRanges = append(destinationRanges, Range{
+					seedRange.start,
+					seedRange.length,
+				})
+			}
+		}
+		sourceRanges = destinationRanges
 	}
 
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
+	fmt.Println(minRange(sourceRanges...).start)
 
-	for location := range ch {
-		minimumLocation = min(location, minimumLocation)
-	}
-
-	fmt.Println(minimumLocation)
 	fmt.Println("Time elapsed: ", time.Since(startTime))
 }
 
